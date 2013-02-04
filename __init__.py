ce11a7f820c84713e012bd07b62b4697dcf798e1
Dmitry Timofeev <dimkat@gmail.com>
2013-02-04 16:41:58 +0400

    Init commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from pysimplelogs import Simplelog, Transplant
+
+simplelog = Simplelog("http://127.0.0.1:5000/")
+simplelog.init_levels()
---
+++ pysimplelogs.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+import requests
+import time
+import json
+import os
+
+SLEEP_TIME = 0.1
+NUMBER_OF_ATTEMPTS = 5
+CONNECTION_TIMEOUT = 3
+URL = "http://127.0.0.1:5000/api/entry/"
+
+class Worker:
+    def __init__(self, level, owner, data, tags = []):
+        self.level = level
+        self.owner = owner
+        self.data = data
+        self.tags = tags
+
+    def send(self, url=URL):
+        data_for_posting = {'level': self.level,
+                            'owner': self.owner,
+                            'data': self.data,
+                            'tags': self.tags}
+        attempts = NUMBER_OF_ATTEMPTS
+        request = False
+        while attempts:
+            try:
+                request = requests.post(url, data=data_for_posting, timeout = CONNECTION_TIMEOUT)
+                break
+            except:
+                attempts -= 1
+                time.sleep(SLEEP_TIME)
+        return request
+
+
+class Transplant:
+    def __init__(self, method, host, method_name=None):
+        self.host = host
+        self.method = method
+        self.method_name = method_name or method.__name__
+        setattr(host, method_name or method.__name__, self)
+
+    def __call__(self, *args, **kwargs):
+        nargs = [self]
+        nargs.extend(args)
+        return apply(self.method, nargs, kwargs)
+
+def get_levels_list(url):
+    return json.loads(requests.get(os.path.join(url, "api", "level"), timeout = CONNECTION_TIMEOUT).content)
+
+class Simplelog:
+    def __init__(self, url):
+        self.url = url
+
+    def init_levels(self):
+        levels = get_levels_list(self.url)['level']
+        for level in levels:
+            Transplant(send, Simplelog, method_name=level)
+
+def send(self, owner, message, tags=[]):
+    # Here i should add multiprocessing, but with sync processes or with "waiting" flag.
+    worker = Worker(self.method_name, owner, message, tags)
+    response = worker.send()
+    return json.loads(response.content)
