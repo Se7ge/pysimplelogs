18f84be014d3ed22cdad60b126d66f1e8ace06c8
Dmitry Timofeev <dimkat@gmail.com>
2013-02-05 19:58:55 +0400

    First working version.

--- pysimplelogs.py
+++ pysimplelogs.py
@@ -6,10 +6,10 @@ import time
 import json
 import os
 
-SLEEP_TIME = 0.1
-NUMBER_OF_ATTEMPTS = 5
-CONNECTION_TIMEOUT = 3
-URL = "http://127.0.0.1:5000/api/entry/"
+SLEEP_TIME = 0.1 # Time between attempts.
+NUMBER_OF_ATTEMPTS = 5 # Attempts.
+CONNECTION_TIMEOUT = 3 # Connections timeout in seconds.
+URL = "http://10.2.0.182/" # Main API URL.
 
 class Worker:
     def __init__(self, level, owner, data, tags = []):
@@ -19,6 +19,8 @@ class Worker:
         self.tags = tags
 
     def send(self, url=URL):
+        url = os.path.join(url, "api", "entry/")
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         data_for_posting = {'level': self.level,
                             'owner': self.owner,
                             'data': self.data,
@@ -27,7 +29,10 @@ class Worker:
         request = False
         while attempts:
             try:
-                request = requests.post(url, data=data_for_posting, timeout = CONNECTION_TIMEOUT)
+                request = requests.post(url,
+                    data=json.dumps(data_for_posting),
+                    headers=headers,
+                    timeout = CONNECTION_TIMEOUT)
                 break
             except:
                 attempts -= 1
@@ -48,7 +53,7 @@ class Transplant:
         return apply(self.method, nargs, kwargs)
 
 def get_levels_list(url):
-    return json.loads(requests.get(os.path.join(url, "api", "level"), timeout = CONNECTION_TIMEOUT).content)
+    return json.loads(requests.get(os.path.join(url, "api", "level/"), timeout = CONNECTION_TIMEOUT).content)
 
 class Simplelog:
     def __init__(self, url):
@@ -59,8 +64,8 @@ class Simplelog:
         for level in levels:
             Transplant(send, Simplelog, method_name=level)
 
-def send(self, owner, message, tags=[]):
-    # Here i should add multiprocessing, but with sync processes or with "waiting" flag.
-    worker = Worker(self.method_name, owner, message, tags)
-    response = worker.send()
-    return json.loads(response.content)
+def send(self, owner, data, tags=[]):
+    worker = Worker(self.method_name, owner, data, tags)
+    #TODO Add queue.
+    multiprocessing.Process(target=worker.send).start()
+    return
