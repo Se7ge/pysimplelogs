48ecfd59e667241f9828af42d094bc61bf71cf08
Dmitry Timofeev <dimkat@gmail.com>
2013-02-15 13:31:21 +0300

    Default levels and removing URL from config.py.

--- pysimplelogs.py
+++ pysimplelogs.py
@@ -6,17 +6,19 @@ import time
 import json
 import os
 
-from config import SLEEP_TIME, NUMBER_OF_ATTEMPTS, CONNECTION_TIMEOUT, URL
+from config import SLEEP_TIME, NUMBER_OF_ATTEMPTS, CONNECTION_TIMEOUT
+
 
 class Worker:
-    def __init__(self, level, owner, data, tags = []):
+    def __init__(self, level, owner, data, url, tags=[]):
         self.level = level
         self.owner = owner
         self.data = data
         self.tags = tags
+        self.url = url
 
-    def send(self, url=URL):
-        url = os.path.join(url, "api", "entry/")
+    def send(self):
+        url = self.url + '/api/entry/'
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         data_for_posting = {'level': self.level,
                             'owner': self.owner,
@@ -27,9 +29,9 @@ class Worker:
         while attempts:
             try:
                 request = requests.post(url,
-                    data=json.dumps(data_for_posting),
-                    headers=headers,
-                    timeout = CONNECTION_TIMEOUT)
+                                        data=json.dumps(data_for_posting),
+                                        headers=headers,
+                                        timeout=CONNECTION_TIMEOUT)
                 break
             except:
                 attempts -= 1
@@ -38,9 +40,10 @@ class Worker:
 
 
 class Transplant:
-    def __init__(self, method, host, method_name=None):
+    def __init__(self, method, host, url, method_name=None):
         self.host = host
         self.method = method
+        self.url = url
         self.method_name = method_name or method.__name__
         setattr(host, method_name or method.__name__, self)
 
@@ -49,20 +52,27 @@ class Transplant:
         nargs.extend(args)
         return apply(self.method, nargs, kwargs)
 
+
 def get_levels_list(url):
-    return json.loads(requests.get(os.path.join(url, "api", "level/"), timeout = CONNECTION_TIMEOUT).content)
+    try:
+        levels = json.loads(requests.get(os.path.join(url + "/api/level/"), timeout=CONNECTION_TIMEOUT).content)
+    except:
+        levels = {u'level': [u'critical', u'error', u'warning', u'notice', u'info', u'debug']}
+    return levels
+
 
 class Simplelog:
     def __init__(self, url):
         self.url = url
+        self.init_levels()
 
     def init_levels(self):
         levels = get_levels_list(self.url)['level']
         for level in levels:
-            Transplant(send, Simplelog, method_name=level)
+            Transplant(send, Simplelog, url=self.url, method_name=level)
 
 def send(self, owner, data, tags=[]):
-    worker = Worker(self.method_name, owner, data, tags)
+    worker = Worker(self.method_name, owner, data, self.url, tags)
     #TODO Add queue.
     multiprocessing.Process(target=worker.send).start()
     return
