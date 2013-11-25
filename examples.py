1197b4b5e5e53ee49e35321b025a5e9cf48b06d7
SAntipov <santipov@korusconsulting.ru>
2013-11-25 14:14:54 +0400

    Example for logger

--- examples.py
+++ examples.py
@@ -1,1 +1,14 @@
-# TODO: add examples
+# -*- coding: utf-8 -*-
+from logger import SimpleLogger
+
+# Logger example
+config = dict()
+config['SIMPLELOGS_URL'] = 'http://192.168.1.102'  # Адрес системы журналирования
+config['MODULE_NAME'] = 'elreg'  # Код журналируемой системы для инициализации логера: logging.getLogger(module_name)
+config['OWNER'] = dict(name=u'Электронная регистратура', version='2.2.10')  # Owner для системы журналирования
+config['DEBUG'] = False  # Флаг, соответствующий debug_mode приложения # write to log all unhandled exceptions if not DEBUG mode
+
+logger = SimpleLogger.get_logger(config.get('SIMPLELOGS_URL'),
+                                 config.get('MODULE_NAME'),
+                                 config.get('OWNER'),
+                                 config.get('DEBUG'))
